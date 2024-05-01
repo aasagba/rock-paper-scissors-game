b300a84d6b8bfadc74dbceccd465bfe22de2cd29
unknown <aasagba@gmail.com>
2024-05-01 15:53:08 +0100

    add header component to allow 2 players to enter their names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { VM } from './types';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   vm$: Observable<VM>;
+  player1: string;
+  player2: string;
+  startGame: boolean;
+
+  onStartGame(names: { player1: string; player2: string }) {
+    this.player1 = names.player1;
+    this.player2 = names.player2;
+
+    this.startGame = Boolean(this.player1 && this.player2);
+  }
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent implements OnInit {
+  form: FormGroup;
+
+  @Output() playerNames: EventEmitter<{ player1: string; player2: string }> =
+    new EventEmitter<{ player1: string; player2: string }>();
+
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  setupForm() {
+    this.form = new FormGroup({
+      player1: new FormControl(null, Validators.required),
+      player2: new FormControl(null, Validators.required),
+    });
+  }
+
+  startGame() {
+    if (!this.form.valid) return;
+
+    const player1 = this.form.get('player1').value;
+    const player2 = this.form.get('player2').value;
+
+    this.playerNames.emit({
+      player1,
+      player2,
+    });
+  }
+}
